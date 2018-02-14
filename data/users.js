d83bf547f752803b65ad63f9d06dd71c0c1c1a1a
Pulkit Valecha <pulkitvalecha98@gmail.com>
2018-02-14 17:20:13 +0530

    cookieParser and users

--- data/users.js
+++ data/users.js
@@ -1,6 +1,32 @@
+const uid = require('uid')
 const users = {}
+const tokens = {}
 
+exports = module.exports =  {
 
-exports = exports.module = {
-    users
+    addUser(username, password){
+        users[username] = {
+            username,
+            password
+        }
+    },
+
+    getUser(username){
+        return users[username]
+    },
+
+    getUserWithToken(token){
+        return users[tokens[token]]
+    },
+
+    createToken(username){
+        let token = uid(10)
+        tokens[token] = username
+        return token
+    },
+
+    removeToken(token){
+        delete tokens[token]
+    }
+
 }
